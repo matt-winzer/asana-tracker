dd120150434ca938b19d322b20db908cfe4e0150
matt-winzer <matt.winzer@gmail.com>
2016-12-13 14:44:23 -0700

    added route for index.hbs and kneq queries to get all asana data, modified index.hbs to display ul of asanas

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,43 @@
 var express = require('express');
 var router = express.Router();
 
-// *** ADD knex ***
-// var knex = require('../db/knex');
+var knex = require('../db/knex');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  return knex('asana')
+    .then((asanas) => {
+      console.log(asanas);
+      res.render('index', { asanas: asanas });
+    });
 });
+
+router.get('/asanas', function(req, res) {
+  return knex('asana')
+    .then((asanas) => {
+      res.json(asanas);
+    });
+});
+
+
+// Berto's Breakout code
+router.post('/', function (req, res) {
+  knex('asana')
+    .insert(req.body)
+    .returning('id')
+    .then((id) => {
+      res.redirect('/' + id);
+    });
+});
+
+router.get('/:id', function (req, res) {
+  knex('asana')
+    .where('id', req.params.id)
+    .then((asana) => {
+      res.render('asana_by_id', {title: asana.name});
+    });
+
+});
+
 
 module.exports = router;
