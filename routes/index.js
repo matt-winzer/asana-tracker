cda2365b4b46b12180ba96e1d285241e2452837c
matt-winzer <matt.winzer@gmail.com>
2016-12-13 12:47:01 -0700

    seeded asana tabe in yoga database

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
+// *** ADD knex ***
+// var knex = require('../db/knex');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
--- seeds/01_asana.js
+++ seeds/01_asana.js
@@ -1,13 +1,58 @@
-
 exports.seed = function(knex, Promise) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex.raw('TRUNCATE asana; ALTER SEQUENCE asana_id_seq restart with 1;')
     .then(function () {
-      return Promise.all([
-        // Inserts seed entries
-        knex('table_name').insert({id: 1, colName: 'rowValue1'}),
-        knex('table_name').insert({id: 2, colName: 'rowValue2'}),
-        knex('table_name').insert({id: 3, colName: 'rowValue3'})
-      ]);
+      const asanas = [{
+        name: 'anahatasana',
+        primary_target: 'upper back, heart',
+        duration: 2.5,
+        difficulty: 5
+      }, {
+        name: 'butterfly',
+        primary_target: 'lower back, hips',
+        duration: 4,
+        difficulty: 3
+      }, {
+        name: 'half butterfly',
+        primary_target: 'lower back, hamstrings',
+        duration: 3,
+        difficulty: 4
+      }, {
+        name: 'caterpillar',
+        primary_target: 'back',
+        duration: 3,
+        difficulty: 4
+      }, {
+        name: 'childs pose',
+        primary_target: 'back, stomach',
+        duration: 4,
+        difficulty: 1
+      }, {
+        name: 'dragon',
+        primary_target: 'hips, groin',
+        duration: 2,
+        difficulty: 5
+      }, {
+        name: 'sphinx',
+        primary_target: 'sacrum, low back',
+        duration: 3,
+        difficulty: 2
+      }, {
+        name: 'frog',
+        primary_target: 'groin',
+        duration: 2.5,
+        difficulty: 4
+      }, {
+        name: 'saddle',
+        primary_target: 'sacrum, low back',
+        duration: 3,
+        difficulty: 5
+      }, {
+        name: 'sleeping swan',
+        primary_target: 'hips',
+        duration: 4,
+        difficulty: 4
+      }];
+
+      return knex('asana').insert(asanas);
     });
 };
