615df4ed6d9031d02a614498e3674321acfd7496
matt-winzer <matt.winzer@gmail.com>
2016-12-14 11:35:06 -0700

    added PUT route and edit.hbs file, functionality working

--- routes/index.js
+++ routes/index.js
@@ -18,11 +18,28 @@ router.get('/asanas', function(req, res) {
     });
 });
 
+// GET create new resource page
 router.get('/new', function(req, res) {
   res.render('new');
 });
 
-// Berto's Breakout code
+// GET edit resource page
+router.get('/:id/edit', function(req, res) {
+  knex('asana')
+    .where('id', req.params.id)
+    .first()
+    .then((asana) => {
+      res.render('edit', {
+        id: asana.id,
+        name: asana.name,
+        primary_target: asana.primary_target,
+        duration: asana.duration,
+        difficulty: asana.difficulty
+      });
+    });
+});
+
+// POST new resource
 router.post('/', function (req, res) {
   knex('asana')
     .insert(req.body)
@@ -32,12 +49,12 @@ router.post('/', function (req, res) {
     });
 });
 
+// GET single resource
 router.get('/:id', function (req, res) {
   knex('asana')
     .where('id', req.params.id)
     .first()
     .then((asana) => {
-      console.log(asana);
       res.render('asana_by_id', {
         id: asana.id,
         name: asana.name,
@@ -48,6 +65,7 @@ router.get('/:id', function (req, res) {
     });
 });
 
+// DELETE route
 router.post('/:id', (req, res) => {
   let id = req.params.id;
   knex('asana')
@@ -55,6 +73,19 @@ router.post('/:id', (req, res) => {
     .del()
     .then((result) => {
       res.redirect('/');
+    });
+});
+
+// PUT route
+router.post('/:id/edit', (req, res) => {
+  let id = req.params.id;
+  let edit = req.body;
+  knex('asana')
+    .where('id', id)
+    .update(edit)
+    .returning('id')
+    .then((id) => {
+      res.redirect('/' + id);
     });
 });
 
